dc055f8cc83a31ad5fea2d819b232b9a76783879
Caio Guerras <84776086+oCaioGuerras@users.noreply.github.com>
2023-03-22 12:18:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,81 @@
-import os
-import pymongo
-from pymongo import MongoClient
-from dotenv import load_dotenv
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
-import json
-import datetime
-import re
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-from collections.abc import MutableMapping
+from bs4 import BeautifulSoup
+import telegram
+import asyncio
 
-#class MyDict(MutableMapping):
-# Implementação da classe
 
-# Load environment variables
-load_dotenv()
+async def get_indeed_jobs():
+    url = 'https://www.indeed.com.br/empregos-em-Maca%C3%A9,-RJ'
 
-# MongoDB Configuration
-MONGODB_URI = os.getenv("MONGODB_URI")
-MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
-MONGODB_COLLECTION_NAME = os.getenv("MONGODB_COLLECTION_NAME")
-
-client = MongoClient(MONGODB_URI)
-db = client[MONGODB_DB_NAME]
-col = db[MONGODB_COLLECTION_NAME]
-
-# Telegram API Configuration
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-bot = Client("job_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-def get_jobs_by_location(location):
-    url = f"http://api.indeed.com/ads/apisearch?publisher={os.getenv('PUBLISHER_ID')}&q=python&l={location}&sort=&radius=&st=&jt=&start=&limit=50&fromage=&filter=&latlong=1&co=br&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
     response = requests.get(url)
-    data = json.loads(response.text)
-    return data["results"]
-def send_job_list(chat_id, job_list):
-    for job in job_list:
-        if col.find_one({"jobkey": job["jobkey"]}) is None:
-            col.insert_one(job)
-            message_text = f"""
-*{job['jobtitle']}*
-_{job['company']}_
-{job['formattedLocation']}
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-{job['snippet']}
+    job_listings = soup.find_all('div', class_='jobsearch-SerpJobCard unifiedRow row result')
 
-*{job['url']}*
-"""
-            message_text = re.sub("<.*?>", "", message_text)
-            sentiment_score = SentimentIntensityAnalyzer().polarity_scores(message_text)["compound"]
-            sentiment_label = "Ruim" if sentiment_score < -0.1 else "Ótima" if sentiment_score > 0.1 else "Boa"
-            message_text += f"\nClassificação de sentimento: {sentiment_label}"
-            bot.send_message(chat_id=chat_id, text=message_text, parse_mode="Markdown")
-@bot.on_message(filters.command("start"))
-def start(bot, update):
-    chat_id = update.chat.id
-    message_text = "Olá, eu sou um bot que envia vagas de emprego para programadores Python no Brasil. Para começar, por favor, envie sua localização."
-    bot.send_message(chat_id=chat_id, text=message_text)
+    jobs = []
+    for job_listing in job_listings:
+        title = job_listing.find('h2', class_='title').text.strip()
+        company = job_listing.find('span', class_='company').text.strip()
+        location = job_listing.find('span', class_='location').text.strip()
+        link = 'https://www.indeed.com.br' + job_listing.find('a')['href']
+        job = f"{title}\n{company}\n{location}\n{link}\n"
+        jobs.append(job)
 
-@bot.on_message(filters.location)
-def location_received(bot, update):
-    chat_id = update.chat.id
-    latitude = update.location.latitude
-    longitude = update.location.longitude
+    return jobs
 
-    geocode_url = f"https://geocode.xyz/{latitude},{longitude}?geoit=json"
-    geocode_response = requests.get(geocode_url)
-    geocode_data = json.loads(geocode_response.text)
 
-    city = geocode_data["city"]
-    state = geocode_data["state"]
-    location = f"{city}, {state}"
-    job_list = get_jobs_by_location(location)
-    send_job_list(chat_id, job_list)
+async def get_linkedin_jobs():
+    url = 'https://www.linkedin.com/jobs/search/?keywords=&location=Maca%C3%A9%2C%20Rio%20de%20Janeiro%2C%20Brasil&geoId=104076686&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 
-bot.run()
-    # Iniciar o loop do bot
-app.run()
-    
-if __name__ == "__main__":
-    main()
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    job_listings = soup.find_all('li', class_='result-card job-result-card result-card--with-hover-state')
+
+    jobs = []
+    for job_listing in job_listings:
+        title = job_listing.find('h3', class_='result-card__title').text.strip()
+        company = job_listing.find('a', class_='result-card__subtitle-link job-result-card__subtitle-link').text.strip()
+        location = job_listing.find('span', class_='job-result-card__location').text.strip()
+        link = job_listing.find('a', class_='result-card__full-card-link')['href']
+        job = f"{title}\n{company}\n{location}\n{link}\n"
+        jobs.append(job)
+
+    return jobs
+
+
+async def send_telegram_message(token, chat_id, message):
+    bot = telegram.Bot(token=token)
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+async def main():
+    token = '5701982262:AAEEnZXQr1Z_fdMsGzqLyVhQ5okMNrmi5t4'
+    chat_id = '-1001579445015_683'
+    db_path = 'db.txt'
+
+    try:
+        with open(db_path, 'r') as f:
+            db = f.read().splitlines()
+    except FileNotFoundError:
+        db = []
+
+    indeed_jobs = await get_indeed_jobs()
+    linkedin_jobs = await get_linkedin_jobs()
+
+    new_jobs = [job for job in indeed_jobs + linkedin_jobs if job not in db]
+
+    if new_jobs:
+        message = '\n\n'.join(new_jobs)
+        await send_telegram_message(token, chat_id, message)
+        print(f'{len(new_jobs)} nova(s) vaga(s) encontrada(s)')
+        db += new_jobs
+        with open(db_path, 'w') as f:
+            f.write('\n'.join(db))
+    else:
+        await send_telegram_message(token, chat_id, 'Sem vagas no momento')
+        print('Sem novas vagas')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
